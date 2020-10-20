f3ec3c7fa1418585f6e09b5bb6d9781d958c1f2d
Naciongsc <60244165+Naciongsc@users.noreply.github.com>
2020-10-20 10:34:01 +0800

    Update app.js

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res){
 res.render('index');
 });
