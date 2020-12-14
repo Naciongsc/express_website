0a95c9bc4b4e6b867cf9c868de657a318c5aec99
Naciongsc <60244165+Naciongsc@users.noreply.github.com>
2020-12-14 21:24:56 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,3 @@
-var app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-app.listen(3000);
-console.log('Server is running on port 3000');
-
-app.get('/', function(req, res){
-console.log('Hello World');
-});     
-
-app.get('/', function(req, res){
-res.send('Hello World');
-});
-
-app.set('views', path.join(__dirname, 'views'));
-
-app.set('view engine', 'pug');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', function(req, res){
-res.render('index');
-});
-
-
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
